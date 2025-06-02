84435022b5cc8c0b249335f3427724376486cc5f
CapnKronic <39390141+CapnKronic@users.noreply.github.com>
2025-06-02 05:23:40 -0500

    refac

---
+++ components/header.js
@@ -0,0 +1,29 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+
+    connectedCallback() {
+        this.innerHTML = `
+        <header id="header" class="header d-flex align-items-center">
+            <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+                <a href="../index.html" class="logo d-flex align-items-center">
+                <h1>Mackey Construction<span>.</span></h1>
+                </a>
+                <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+                <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+                <nav id="navbar" class="navbar">
+                    <ul>
+                        <li><a href="../index.html">Home</a></li>
+                        <li><a href="site/about.html" class="active">About</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>`;
+    }
+}
+
+
+
+customElements.define('header-component', Header);
