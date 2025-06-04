e6ef148962395110ffeaa2a76092fed6e6a3f28e
CapnKronic <39390141+CapnKronic@users.noreply.github.com>
2025-06-04 02:53:07 -0500

    css

--- components/header.js
+++ components/header.js
@@ -9,8 +9,10 @@ class Header extends HTMLElement {
         <header id="header" class="header d-flex align-items-center">
             <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
                 <a href="../index.html" class="logo d-flex align-items-center">
-              <h3 style="white-space: nowrap;color:white; font-weight: 500;">Mackey <span style="font-weight: 100;">Construction</span></h3>
-                
+                <h2>
+                <i class="bi bi-buildings"></i>
+                Mackey <span>Construction</span>
+                </h2>
                 </a>
                 <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
                 <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
