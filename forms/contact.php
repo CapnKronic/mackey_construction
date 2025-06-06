6d66dd4610af73ed8fb392ab474c01b942fa1b7f
CapnKronic <39390141+CapnKronic@users.noreply.github.com>
2025-06-06 08:27:43 -0500

    t

--- forms/contact.php
+++ forms/contact.php
@@ -30,7 +30,7 @@ echo "Request Sent";
 
 $hostname= "";
 $username = "web";
-$password = "SM0kin52581";
+$password = "";
 $db = "jmc";
 
 $dbconnect=mysqli_connect($hostname,$username,$password,$db);
