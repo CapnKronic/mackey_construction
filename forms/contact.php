47f5b55248131ffb58b6471d34be23b8e1fc590a
CapnKronic <39390141+CapnKronic@users.noreply.github.com>
2025-06-06 06:14:26 -0500

    reset

--- forms/contact.php
+++ forms/contact.php
@@ -1,21 +1,17 @@
 <?php
 
-
-$receiving_email_address = 'test@jmackeyconstruction.com';
-// $receiving_email_address = 'info@jmackeyconstruction.com';
+$receiving_email_address = 'info@jmackeyconstruction.com';
 $mail_subject = 'Contact Request: ';
 
 
-  $to = $receiving_email_address;
-  $from_name = $_POST['name'];
-  $from_email = $_POST['email'];
-  $address = 'Adress Placeholder';
-  // $address = $_POST['address'];
-  $subject = $mail_subject . $from_name;
-  $phone = $_POST['phone'];
-  $message = $from_name . "\r\n" . $phone . "\r\n" . $from_email . "\r\n" . $address . "\r\n" . $_POST['message'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+$to = $receiving_email_address;
+$from_name = $_POST['name'];
+$from_email = $_POST['email'];
+$subject = $mail_subject . $from_name;
+$phone = $_POST['phone'];
+$message = $from_name . "\r\n" . $phone . "\r\n" . $from_email . "\r\n" . $_POST['message'];
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 /*
 $contact->smtp = array(
   'host' => 'example.com',
@@ -25,44 +21,7 @@ $contact->smtp = array(
 );
 */
 
-  // echo $to, "\n", $from_name, "\n", $from_email, "\n", $subject, "\n", $message;
+// echo $to, "\n", $from_name, "\n", $from_email, "\n", $subject, "\n", $message;
 mail($to, $subject, $message, "From:" . $from_email);
 echo "Request Sent";
-
-$hostname = '';
-$username = 'kronicjo';
-$password = 'SM0kin52581';
-$db = 'jmc';
-
-$dbconnect = mysqli_connect($hostname, $username, $password, $db);
-
-if ($dbconnect->connect_error) {
-  echo 'NOT CONNECTED ' . $dbconnect->connect_error;
-  die("Database connection failed: " . $dbconnect->connect_error);
-}
-
-// if (isset($_POST['submit'])) {
-  // $user_name=$_POST['username'];
-// $user_password=$_POST['psw'];
-
-
-
-  $query = "INSERT INTO web_contacts (contact, phone, email, addr, notes)
-VALUES ('$from_name', '$phone', '$from_email', '$address', '$message')";
-
-  if ($dbconnect->query($query) === TRUE) {
-    echo "New record created successfully";
-  // }
-
-  $conn->close();
-
-
-
-
-
-
-
-
-
-}
 ?>
